4da1444bce019cef91d5c3fee3340a8dd6d875c2
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-03-07 13:18:20 +0100

    First task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,32 +1,13 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
 
-console.log(customer.getBalance());
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const massiveItems = categories.querySelectorAll(".item");
+const numberCategories = massiveItems.length;
+console.log(`Number of categories: ${numberCategories}`) 
 
+massiveItems.forEach(item => { textAndNumber(item) }) 
+
+    function textAndNumber(item) {
+    const namesElements = item.querySelector("h2").textContent;
+    const numbersItems = item.querySelectorAll("li").length;
+    console.log(`Category: ${namesElements}`);
+    console.log(`Elements: ${numbersItems}`);
+}
