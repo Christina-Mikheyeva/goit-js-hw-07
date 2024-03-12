0f5fe13c32efb67ddbb2ab8cbb05c47a71efd770
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-03-12 20:37:42 +0100

    First task done

---
+++ js/task-1.js
@@ -0,0 +1,24 @@
+
+const massiveItems = document.querySelectorAll(".item");
+const numberCategories = massiveItems.length;
+console.log(`Number of categories: ${numberCategories}`) 
+
+massiveItems.forEach(item => { textAndNumber(item) }) 
+
+    function textAndNumber(item) {
+    const namesElements = item.querySelector("h2").textContent;
+    const numbersItems = item.querySelectorAll("li").length;
+    console.log(`Category: ${namesElements}`);
+    console.log(`Elements: ${numbersItems}`);
+
+    }
+
+// Decoration 
+    
+const massiveLi = document.querySelectorAll("li");
+
+for (const element of massiveLi) { 
+    if (!element.classList.contains("item")) {
+        element.classList.add("noitem")
+    }
+}
---
+++ js/task-4.js
@@ -0,0 +1,30 @@
+const submit = document.querySelector(".login-form");
+submit.addEventListener("submit", submitAction)
+
+function submitAction(event) {
+    event.preventDefault()
+
+    const { email, password } = event.target.elements;
+
+    const clientEmail = email.value.trim();
+    const clientPassword = password.value.trim();
+
+    if ( clientEmail === "" || clientPassword === "") {
+    alert ('All form fields must be filled in')
+    }
+
+    const form = {
+      email: clientEmail,
+      password: clientPassword
+    }
+
+    console.log(form);
+    event.target.reset()
+}
+
+
+
+
+    
+
+
